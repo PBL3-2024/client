bc319fe1ab6ecd5c296a5dffed512e12ca096a05
Satvik <>
2024-06-24 23:18:40 +0900

    explore

--- src/views/ExploreAllOccupationView.vue
+++ src/views/ExploreAllOccupationView.vue
@@ -1,3 +1,5 @@
+
+
 <script setup lang="ts">
 import { ref, onMounted, watch } from 'vue';
 import { fetchChildOccupation, fetchOccupation, type Occupation } from '@/api/occupation';
@@ -8,6 +10,11 @@ import OccupationBreadcrumb from '@/components/OccupationBreadcrumb.vue';
 import Chart from 'primevue/chart';
 import { fetchUnemployment } from '@/api/unemployment';
 import { fetchEmployment } from '@/api/employment';
+import Tabs from 'primevue/tabs';
+import TabList from 'primevue/tablist';
+import Tab from 'primevue/tab';
+import TabPanels from 'primevue/tabpanels';
+import TabPanel from 'primevue/tabpanel';
 
 const route = useRoute();
 const router = useRouter();
@@ -22,6 +29,8 @@ const lineChartOptions = ref({});
 const lineChartData2 = ref({});
 const lineChartOptions2 = ref({});
 
+const showTabs = ref<boolean>(false); 
+
 const refreshContent = async () => {
   const socCode = route.params.socCode as string;
   const occupation = (await fetchOccupation(socCode)).data;
@@ -34,17 +43,27 @@ const refreshContent = async () => {
   } else {
     children.value = [];
   }
+  
+  if (socCode === '00-0000') {
+    showTabs.value = false;
+  } else {
+    showTabs.value = socCode.endsWith('0');
+  }
 };
 
 const selectOccupation = (socCode: string) => {
   if (socCode) {
     router.push(`/occupations/${socCode}`);
+    if (socCode === '00-0000') {
+      showTabs.value = false;
+    } else {
+      showTabs.value = socCode.endsWith('00');
+    }
   } else {
     router.push('/');
   }
 };
 
-// Fetch unemployment data on component mount
 onMounted(async () => {
   const unemployment = (await fetchUnemployment('00-0000')).data;
   unemployment.unemployment.reverse();
@@ -60,7 +79,6 @@ onMounted(async () => {
   };
 });
 
-// Fetch employment data on component mount
 onMounted(async () => {
   const employment = (await fetchEmployment('00-0000')).data;
   employment.employment.reverse();
@@ -70,16 +88,14 @@ onMounted(async () => {
       label: 'Employment',
       data: employment.employment.map(e => e.value),
       fill: false,
-      borderColor: 'rgb(192, 75, 75)', // Adjusted color for employment data
+      borderColor: 'rgb(192, 75, 75)',
       tension: 0.1
     }]
   };
 });
 
-// Watch route changes and refresh content
 watch(route, refreshContent);
 
-// Initial fetch and refresh content on component mount
 onMounted(refreshContent);
 
 </script>
@@ -93,12 +109,59 @@ onMounted(refreshContent);
       :soc-code="route.params.socCode as string"
       @occupation-selected="selectOccupation"
     />
-    
-    <!-- Display first chart for Unemployment -->
-    <Chart type="line" :data="lineChartData" :options="lineChartOptions" class="w-full md:w-[30rem]" />
+  
+    <Tabs v-if="showTabs" value="0">
+      <TabList>
+        <Tab value="0">Information</Tab>
+        <Tab value="1">News</Tab>
+        <Tab value="2">Learning</Tab>
+        <Tab value="3">Certifications</Tab>
+        <Tab value="4">Jobs</Tab>
+        <Tab value="5">Reports</Tab>
+      </TabList>
+      <TabPanels>
+        <TabPanel value="0">
+          <p class="m-0">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </TabPanel>
+        <TabPanel value="1">
+          <p class="m-0">
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim
+            ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Consectetur, adipisci velit, sed quia non numquam eius modi.
+          </p>
+        </TabPanel>
+        <TabPanel value="2">
+          <p class="m-0">
+            At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa
+            qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus.
+          </p>
+        </TabPanel>
+        <TabPanel value="3">
+          <p class="m-0">
+            At vero eos et accusamus et is praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa
+            qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus.
+          </p>
+        </TabPanel>
+        <TabPanel value="4">
+          <p class="m-0">
+            At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa
+            qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum qa nobis est eligendi optio cumque nihil impedit quo minus.
+          </p>
+        </TabPanel>
+        <TabPanel value="5">
+          <p class="m-0">
+            At vero eos et accusamus et iusto odio  laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus.
+          </p>
+        </TabPanel>
+      </TabPanels>
+    </Tabs>
 
-    <!-- Display second chart for Employment -->
-    <Chart type="line" :data="lineChartData2" :options="lineChartOptions2" class="w-full md:w-[30rem]" />
+
+    <Chart v-if="route.params.socCode === '00-0000'" type="line" :data="lineChartData" :options="lineChartOptions" class="w-full md:w-[30rem]" />
+
+    <Chart v-if="route.params.socCode === '00-0000'" type="line" :data="lineChartData2" :options="lineChartOptions2" class="w-full md:w-[30rem]" />
 
     <h2>{{ title }}</h2>
     <p>{{ description }}</p>
