187021985e7071afbdaa210745f356d9d341814c
Jesse Grabowski <business@jessegrabowski.com>
2024-06-25 14:41:13 +0900

    Implement subviews

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import ExploreAllOccupationView from '../views/ExploreAllOccupationView.vue'
 import SelectMajorAndMinorGroupView from '@/views/SelectMajorAndMinorGroupView.vue'
+import InformationView from '@/views/InformationView.vue'
+import LearningView from '@/views/LearningView.vue'
+import CertificationView from '@/views/CertificationView.vue'
+import JobView from '@/views/JobView.vue'
+import ManageDemandView from '@/views/ManageDemandView.vue'
+import ReportTypeView from '@/views/ReportTypeView.vue'
 import NewsView from '@/views/NewsView.vue'
 
 
@@ -10,15 +16,33 @@ const router = createRouter({
     {
       path: '/occupations/:socCode',
       name: 'occupations',
-      component: ExploreAllOccupationView
+      component: ExploreAllOccupationView,
+      children: [{
+        path: 'information',
+        component: InformationView
+      }, {
+        path: 'news',
+        component: NewsView
+      }, {
+        path: 'learning',
+        component: LearningView
+      }, {
+        path: 'certifications',
+        component: CertificationView
+      }, {
+        path: 'jobs',
+        component: JobView
+      }, {
+        path: 'demand',
+        component: ManageDemandView
+      }, {
+        path: 'reports',
+        component: ReportTypeView
+      }]
     }, {
       path: '/select/:socCode',
       name: 'select',
       component: SelectMajorAndMinorGroupView
-    }, {
-      path: '/occupations/:socCode/news',
-      name: 'news',
-      component: NewsView
     },
   ]
 })
