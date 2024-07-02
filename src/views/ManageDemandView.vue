b84e547c135b9d1545e21d66b024a93ca1446955
Hiroyuki <>
2024-07-02 15:52:24 +0900

    Implemented ManageDemandView and made small changes to HomePageView

--- src/views/ManageDemandView.vue
+++ src/views/ManageDemandView.vue
@@ -1,11 +1,40 @@
 <script setup lang="ts">
+import { ref, onMounted, watch } from 'vue'
+import { fetchDemand, editDemand, type Demand } from '@/api/demand'
+import { useRoute, useRouter } from 'vue-router'
 
+
+const route = useRoute();
+
+const demand = ref<Demand>({
+  "socCode": "00-0000",
+  "value": 0
+});
+
+const refreshContent = async function() {
+  const socCode = route.params.socCode as string;
+  demand.value = (await fetchDemand(socCode)).data;
+}
+
+const submit = async function() {
+  const socCode = route.params.socCode as string;
+  await editDemand(socCode, demand.value);
+}
+
+onMounted(refreshContent);
+watch(route, refreshContent);
 
 </script>
 
 <template>
   <main>
-    <h1>Certification Information</h1>
+    <h1>Manage Demand</h1>
+    <p>{{ demand?.socCode }}</p>
+    <p>{{ demand?.value }}</p>
+    <input type="number" v-model="demand.value" />
+    <button @click="submit">Submit</button>
+
+
 
   </main>
 </template>
